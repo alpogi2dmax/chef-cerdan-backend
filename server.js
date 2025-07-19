eef408eac339b2f7809913ee17a2c635e2ed3898
Al Cerdan Lico <alcerdan@yahoo.com>
2025-07-19 11:45:35 -0700

    correct type in port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const { HfInference } = require('@huggingface/inference')
 dotenv.config()
 
 const app = express()
-const port = process.ENV.port || 5000
+const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
